0e9b5bbba704fdf34420b74e6bef4e2977b227ff
JoeyMartisPXL <73548550+JoeyMartisPXL@users.noreply.github.com>
2024-03-26 09:42:42 +0100

    First upload of project

---
+++ Stage - RDG/Stage - RDG/Chicken.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Stage___RDG
+{
+    public class Chicken(long id, string name, string assignedGender, string dateOfBirth)
+    {
+        public long Id { get; private set; } = id;
+        public string Name { get; private set; } = name;
+        public string AssignedGender { get; private set; } = assignedGender;
+        public string DateOfBirth { get; private set; } = dateOfBirth;
+    }
+}
---
+++ Stage - RDG/Stage - RDG/Program.cs
@@ -0,0 +1,35 @@
+﻿using NameGenerator.Generators;
+using System.IO;
+
+namespace Stage___RDG
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Start generating data...");
+            string pathSmall = @"c:\Users\JoeyM\Desktop\StageSteria\CSV\testfile.csv";
+            RealNameGenerator generator = new();
+            long amount = 1000000;
+            using StreamWriter outputFile = File.AppendText(pathSmall);
+            try
+            {
+                outputFile.WriteLine($"id, name, gender, date_of_birth");
+                for (long id = 0; id < amount; id++)
+                {
+                    Chicken chicken = ChickenGenerator.CreateChicken(id, generator.Generate().Split(' ')[0]);
+                    outputFile.WriteLine($"{chicken.Id}, {chicken.Name}, {chicken.AssignedGender}, {chicken.DateOfBirth}");
+                    Console.WriteLine("Still need to do: " + (amount - id) + " chickens.");
+                }
+                
+            }
+            catch
+            {
+
+                Console.WriteLine("Something went wrong.");
+            }
+
+
+        }
+    }
+}
